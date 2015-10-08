5db7687703964e3504d0ed50cb2d9b977b347cf4
Kelly Howard <kellybhoward@gmail.com>
2015-10-07 19:20:05 -0700

    updated file with better min/max function

--- sort.php
+++ sort.php
@@ -38,7 +38,7 @@
 	            echo $result;
 	            $time_end = microtime(true);
 			    $time = $time_end - $time_start;
-			    echo "<br><br>Did nothing in " . $time . " seconds\n";
+			    echo "<br><br>Did sort in " . $time . " seconds\n";
 			?>
 		</div>
 		<div id="both_max_min">
@@ -47,28 +47,35 @@
 				function selection_sort_both($array){
 	            	for ($i=0; $i < count($array); $i++){
 	            		$min = $array[$i];
-	            		$max = $array[count($array)-1];
-	            		for ($j = 0; $j < count($array); $j++){
-	            			if($array[$j] < $min){
-	            				$min = $array[$j];
-	            			} else if ($array[$j] > $max){
-	            				$max = $array[$j];
-	            			}
-	            		}
-	            		$tempMin = $array[$i];
-	            		$tempMax = $array[count($array)-1];
-	            		$array[$i] = $min;
-	            		$array[count($array)-1] = $max;
+						$max = $array[$i];
+						$minIndex = $i;
+						$maxIndex = $i;
+		            	for ($j=$i; $j < count($array); $j++) { 
+							if ($min > $array[$j]){
+								$min = $array[$j];
+								$minIndex = $j;
+							} else if ($max < $array[$j]){
+								$max = $array[$j];
+								$maxIndex = $j;
+							}
+						}
+					$temp = $array[$i];
+						for ($k=0; $k < count($array); $k++){
+							if ($min == $array[$k]){
+								$array[$k] = $temp;
+							}
+						}
+					$array[$i] = $min;
+					}
 	            	return $array;
-		            }
 	            }
 	            $time_start = microtime(true);
 	            $result = implode(", ", selection_sort_both($sortingArray));
 	            echo $result;
 	            $time_end = microtime(true);
 			    $time = $time_end - $time_start;
-			    echo "<br><br>Did nothing in " . $time . " seconds\n";
-			?>
+			    echo "<br><br>Did sort in " . $time . " seconds\n";
+		    ?>
 		</div>
 	</body>
 </html>
